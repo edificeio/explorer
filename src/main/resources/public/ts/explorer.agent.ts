095c6fda1f497e4f00d6e7d840ec638ed2fc08ad
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-03-23 16:53:01 +0100

    feat: mock exploration of "Mes blogs"

--- src/main/resources/public/ts/explorer.agent.ts
+++ src/main/resources/public/ts/explorer.agent.ts
@@ -1,40 +1,7 @@
-import { AbstractBusAgent, ACTION, CreateFolderParameters, CreateFolderResult, GetContextParameters, GetContextResult, GetResourcesParameters, GetResourcesResult, IContext, ID, RESOURCE } from 'ode-ts-client';
+import { AbstractBusAgent, ACTION, APP, CreateFolderParameters, CreateFolderResult, ERROR_CODE, GetContextParameters, GetContextResult, GetResourcesParameters, GetResourcesResult, IContext, ID, RESOURCE, ResourceType } from 'ode-ts-client';
 import { IHandler } from 'ode-ts-client/dist/ts/foundation/Agent';
-
-let ContextData = {
-	"folders":[]
-   ,"filters":[]
-   ,"orders":[{
-	   "id": "name"
-	  ,"defaultValue": "asc"
-	  ,"name": "fake.key.order.name.asc"
-	}
-   ]
-   ,"actions":[
-		{"id":"comment",  "available":false}
-	   ,{"id":"copy",     "available":false}
-	   ,{"id":"create",   "available":false}
-	   ,{"id":"delete",   "available":false}
-	   ,{"id":"export",   "available":false}
-	   ,{"id":"initialize", "available":true}
-	   ,{"id":"manage",   "available":false}
-	   ,{"id":"move",     "available":false}
-	   ,{"id":"open",     "available":false}
-	   ,{"id":"print",    "available":false}
-	   ,{"id":"publish",  "available":false}
-	   ,{"id":"search",   "available":true}
-	   ,{"id":"share",    "available":false}
-	  ]
-   ,"pagination":{
-	   "startIdx": 0
-	  ,"maxIdx": 22
-	  ,"pageSize": 10
-   }
-   ,"resources":[]
-   ,"preferences":{
-	   "view": "list"
-   }
-  };
+import * as ContextData from "./mocked/GetContext.json";
+import * as SearchBlogFolder000Data from "./mocked/GetSearchBlogFolder000.json";
 
 /* Ce code est une recopie du code de l'agent mocké pour les tests unitaires.
  * Il s'agit d'un agent de gestion de ressources de type Folder.
@@ -56,7 +23,7 @@ class ExplorerAgent extends AbstractBusAgent {
 
     onInitialize( parameters:GetContextParameters ): Promise<GetContextResult> {
         return Promise.resolve().then( () => {
-            return ContextData as GetContextResult
+            return ContextData as GetContextResult;
         }).then( ctx => {
             return this.ctx = ctx;
         });
@@ -69,7 +36,21 @@ class ExplorerAgent extends AbstractBusAgent {
     }
 
     onSearch( parameters:GetResourcesParameters ): Promise<GetResourcesResult> {
-        throw new Error("Method not implemented.");
+        //console.log( parameters );
+        // Only Blog and exercizer are supported by the prototype
+        if( parameters.app!==APP.BLOG && parameters.app!==APP.EXERCIZER ) {
+            throw new Error(ERROR_CODE.NOT_SUPPORTED);
+        }
+        if( parameters.types.findIndex((res)=>(res===RESOURCE.BLOG || res===RESOURCE.EXERCISE)) === -1) {
+            throw new Error(ERROR_CODE.NOT_SUPPORTED);
+        }
+        return Promise.resolve().then( () => {
+        // Simulate querying the server.
+            switch(parameters.filters.folder) {
+                case "blogFolder000": return SearchBlogFolder000Data as unknown as GetResourcesResult;
+                default: throw new Error(ERROR_CODE.NOT_SUPPORTED);
+            }
+        });        
     }
 }
 
