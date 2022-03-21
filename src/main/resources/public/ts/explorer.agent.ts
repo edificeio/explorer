ad1288256ebb0404ad082aef5c28a04714393ba1
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-03-21 14:42:19 +0100

    feat: #WB-341, front to back POC

--- src/main/resources/public/ts/explorer.agent.ts
+++ src/main/resources/public/ts/explorer.agent.ts
@@ -1,5 +1,5 @@
 import http from 'axios';
-import { AbstractBusAgent, ACTION, GetContextParameters, GetContextResult, IActionParameters, IActionResult, IContext, IHttp, ISearchResults, ManagePropertiesParameters, ManagePropertiesResult, PROP_KEY, PROP_MODE, PROP_TYPE, RESOURCE, TransportFrameworkFactory } from 'ode-ts-client';
+import { AbstractBusAgent, ACTION, CreateFolderParameters, CreateFolderResult, DeleteParameters, GetContextParameters, GetContextResult, GetSubFoldersResult, IActionParameters, IActionResult, IContext, ID, IHttp, ISearchResults, ManagePropertiesParameters, ManagePropertiesResult, MoveParameters, PROP_KEY, PROP_MODE, PROP_TYPE, RESOURCE, TransportFrameworkFactory } from 'ode-ts-client';
 import { IHandler } from 'ode-ts-client/dist/ts/explore/Agent';
 declare var console:any;
 console.log("explorer agent loading....")
@@ -27,12 +27,63 @@ class ExplorerAgent extends AbstractBusAgent {
     });
 
     protected registerHandlers(): void {
-        this.setHandler( ACTION.INITIALIZE, this.onInit as unknown as IHandler );
-        this.setHandler( ACTION.SEARCH,   	this.onSearch as unknown as IHandler );
-        this.setHandler( ACTION.OPEN,   	this.onOpen as unknown as IHandler );
-        this.setHandler( ACTION.CREATE,   	this.onCreate as unknown as IHandler );
+        this.setHandler( ACTION.INITIALIZE, this.createContext as unknown as IHandler );
+        this.setHandler( ACTION.SEARCH,     this.searchContext as unknown as IHandler );
+        this.setHandler( ACTION.CREATE,     this.createFolder as unknown as IHandler );
+        this.setHandler( ACTION.OPEN,       this.listSubfolders as unknown as IHandler );
+        this.setHandler( ACTION.MOVE,       this.moveFolder as unknown as IHandler );
+        this.setHandler( ACTION.DELETE,     this.deleteFolders as unknown as IHandler );
         this.setHandler( ACTION.MANAGE,     this.onManage as unknown as IHandler );
     }
+
+    /** Create a search context. */
+    createContext( parameters:GetContextParameters ): Promise<GetContextResult> {
+        return this.http.get<GetContextResult>('/explorer/context', {
+            queryParams: this.toQueryParams(parameters)
+        });
+    }
+
+    /** Search / paginate within a search context. */
+    searchContext( parameters:GetContextParameters ): Promise<ISearchResults> {
+        return this.http.get<GetContextResult>('/explorer/resources', {
+            queryParams: this.toQueryParams(parameters)
+        });
+    }
+
+    /** Create a new folder. */
+    createFolder( parameters:CreateFolderParameters ): Promise<CreateFolderResult> {
+        return this.http.post<CreateFolderResult>( '/explorer/folders', this.createFolderToBodyParams(parameters) );
+    }
+
+    /** Move a folder. */
+    moveFolder( parameters:MoveParameters ): Promise<IActionResult> {
+        return this.http.post<IActionResult>( `/explorer/folders/${parameters.folderId}/move`, this.moveToBodyParams(parameters) );
+    }
+
+    /** List subfolders of a parent folder. */
+    listSubfolders( folderId:ID ): Promise<GetSubFoldersResult> {
+        return this.http.get<GetSubFoldersResult>( `/explorer/folders/${folderId}/move` );
+    }
+
+    /** Delete folders and/or resources. */
+    deleteFolders( parameters:DeleteParameters ): Promise<IActionResult> {
+        return this.http.deleteJson<IActionResult>( `/explorer/folders`, parameters );
+    }
+
+    onManage( parameters:ManagePropertiesParameters ): Promise<ManagePropertiesResult> {
+        const res:ManagePropertiesResult = {
+            genericProps:[{
+                key:PROP_KEY.TITLE
+            },{
+                key:PROP_KEY.IMAGE
+            },{
+                key:PROP_KEY.URL
+            }]
+        }
+        return Promise.resolve().then( () => res );
+    }
+
+
     private toQueryParams(p:GetContextParameters):any {
         let ret = {
             application:    p.app,
@@ -51,118 +102,20 @@ class ExplorerAgent extends AbstractBusAgent {
         }
         return ret;
     }
-
-    onSearch( parameters:GetContextParameters ): Promise<ISearchResults> {
-        return this.http.get<GetContextResult>('/explorer/resources', {
-            queryParams: this.toQueryParams(parameters)
-        });
-/*
+    private createFolderToBodyParams(p:CreateFolderParameters) {
         return {
-            folders:[0,1,2,3,4,5,6,7,8,9].map((e,index)=>({
-                childNumber: index,
-                id: "folder-"+index,
-                name:"Folder "+index,
-                type: ""
-            })),
-            pagination:{
-                pageSize: 10,
-                startIdx: 0
-            },
-            resources:[0,1,2,3,4,5,6,7,8,9].map((e,index)=>({
-                id: "blog-0"+index,
-                name:"Blog 0"+index,
-                application: "blog",
-                authorId: "auth"+index,
-                authorName:" Auteur "+index,
-                createdAt: "01/01/1990",
-                modifiedAt: "01/01/1990",
-                modifierId: "auth"+index,
-                modifierName: "Auteur "+index,
-                thumbnail: "https://loremflickr.com/240/160/",
-                comments: 10,
-                favorite: true,
-                folderId: ""+index,
-                public: true,
-                shared: true,
-                views: 100
-            }))
-        }
-*/
-    }
-
-    onInit( parameters:GetContextParameters ): Promise<GetContextResult> {
-        return this.http.get<GetContextResult>('/explorer/context', {
-            queryParams: this.toQueryParams(parameters)
-        });
-/*
-        return {
-            actions:[{
-                id: "create",
-                available: true
-            },{
-                id: "open",
-                available: true
-            },{
-                id: "delete",
-                available: true
-            }],
-            filters:[],
-            folders:[0,1,2,3,4,5,6,7,8,9].map((e,index)=>({
-                childNumber: index,
-                id: "folder-"+index,
-                name:"Folder "+index,
-                type: ""
-            })),
-            orders:[{i18n:"name", id:"name"},{i18n:"views", id:"views"},{i18n:"modifiedAt", id:"modifiedAt"}],
-            pagination:{
-                pageSize: 10,
-                startIdx: 0
-            },
-            preferences:{
-                view:"card"
-            },
-            resources:[0,1,2,3,4,5,6,7,8,9].map((e,index)=>({
-                id: "blog-0"+index,
-                name:"Blog 0"+index,
-                application: "blog",
-                authorId: "auth"+index,
-                authorName:" Auteur "+index,
-                createdAt: "01/01/1990",
-                modifiedAt: "01/01/1990",
-                modifierId: "auth"+index,
-                modifierName: "Auteur "+index,
-                thumbnail: "https://loremflickr.com/240/160/",
-                comments: 10,
-                favorite: true,
-                folderId: ""+index,
-                public: true,
-                shared: true,
-                views: 100
-            }))
+            application:    p.app,
+            resourceType:   p.type,
+            parentId:       p.parentId,
+            name:           p.name
         };
-*/
     }
-
-    onOpen( parameters:GetContextParameters ): void {
-        // TODO navigate to the correct URL. 
-    }
-
-    onCreate( parameters:IActionParameters ): Promise<IActionResult> {
-        const res:IActionResult = "/explorer#/edit/new";
-        return Promise.resolve().then( () => res );
-    }
-
-    onManage( parameters:ManagePropertiesParameters ): Promise<ManagePropertiesResult> {
-        const res:ManagePropertiesResult = {
-            genericProps:[{
-                key:PROP_KEY.TITLE
-            },{
-                key:PROP_KEY.IMAGE
-            },{
-                key:PROP_KEY.URL
-            }]
-        }
-        return Promise.resolve().then( () => res );
+    private moveToBodyParams(p:MoveParameters) {
+        return {
+            application:    p.application,
+            resourceIds:    p.resourceIds,
+            folderIds:      p.folderIds
+        };
     }
 }
 
