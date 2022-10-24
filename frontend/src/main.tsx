a18d132cc6376da0295d8736d5c2f9c196dbd202
Clement Creusat <clement.creusat@opendigitaleducation.com>
2022-10-24 14:47:49 +0200

    feat(ode-ts-client): #WB-1086, init ode-ts-client x react

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
-import { StrictMode } from "react";
-
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+/* import { configure } from "ode-ts-client/src/ts/configure/Framework";
+import { session } from "ode-ts-client/src/ts/session/Framework"; */
 
 // import i18n (needs to be bundled ;))
 import App from "./app/App";
@@ -11,7 +12,35 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 
 root.render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 );
+
+/* const handleSession = () => {
+  return session.initialize().then(() => {
+    console.log("init session");
+    return configure.initialize(null, null);
+  });
+}; */
+
+/* session
+  .initialize()
+  .then(() => configure.initialize(null, null))
+  .then(() => {
+    root.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    //root.render(<App />);
+  }); */
+
+/* session.initialize().then(() => {
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+}); */
