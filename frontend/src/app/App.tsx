83b818b8d1e8aad7decd004a5ddddab166428164
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-11-18 18:12:59 +0100

    feat: login/logout helpers

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -5,15 +5,25 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   // TODO useContext
-  const { session } = useOdeBackend(null, null);
+  const { session, login, logout } = useOdeBackend(null, null);
 
   if (session === null) {
-    // TODO améliorer cette gestion d'erreur
     return <div>Impossible de se connecter au backend.</div>;
   }
 
+  const loginForm = session.notLoggedIn ? (
+    <button type="button" onClick={() => login(/* login, mdp */)}>
+      Login
+    </button>
+  ) : (
+    <button type="button" onClick={() => logout()}>
+      Logout
+    </button>
+  );
+
   return (
     <div className="App">
+      <div>{loginForm}</div>
       <Routes>
         <Route index element={<Home />} />
         <Route path="blog" element={<Blog />} />
