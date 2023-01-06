aa6013b963bfd68388efa5ca009e4f95ae04bfff
david-cc <david.caudeli@web-education.net>
2023-01-06 13:30:46 +0100

    refactor: #WB2-92, import TreeNode from ode-react-ui

--- src/hooks/adapters/explorer/types.tsx
+++ src/hooks/adapters/explorer/types.tsx
@@ -1,11 +1,3 @@
-/* TODO exporter TreeNode depuis ode-react-ui/advanced */
-export interface TreeNode {
-  id: string;
-  name: string;
-  section?: boolean;
-  children?: TreeNode[];
-}
-
 export interface Card {
   id: string;
   name: string;
