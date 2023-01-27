5666ee76a3f0b5aec8e8e54a0700b7b99f151265
david-cc <david.caudeli@web-education.net>
2023-01-27 12:45:49 +0100

    feat: #WB2-47, PublishModal add i18n and inputs validation

--- src/features/Actionbar/hooks/usePublishModalOptions.tsx
+++ src/features/Actionbar/hooks/usePublishModalOptions.tsx
@@ -184,8 +184,23 @@ export default function usePublishLibraryModalOptions() {
     { value: "bpr.other", label: i18n("bpr.other") },
   ];
 
-  const ageOptions: number[] = [
-    3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+  const ageOptions: string[] = [
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
   ];
 
   return {
