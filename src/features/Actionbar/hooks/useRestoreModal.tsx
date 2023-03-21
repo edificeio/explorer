ce87b189e7a23e49da6a6b9c942c815dc5b764e1
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2023-03-21 15:46:14 +0100

    feat: #WB2-518, i18n toast restore

--- src/features/Actionbar/hooks/useRestoreModal.tsx
+++ src/features/Actionbar/hooks/useRestoreModal.tsx
@@ -1,3 +1,5 @@
+import { useOdeClient, Alert } from "@ode-react-ui/core";
+import { useHotToast } from "@ode-react-ui/hooks";
 import useExplorerStore from "@store/index";
 
 interface ModalProps {
@@ -5,6 +7,8 @@ interface ModalProps {
 }
 
 export default function useRestoreModal({ onSuccess }: ModalProps) {
+  const { i18n } = useOdeClient();
+  const { hotToast } = useHotToast(Alert);
   // * https://github.com/pmndrs/zustand#fetching-everything
   // ! https://github.com/pmndrs/zustand/discussions/913
   const getIsTrashSelected = useExplorerStore(
@@ -20,6 +24,7 @@ export default function useRestoreModal({ onSuccess }: ModalProps) {
         throw new Error("Cannot restore untrashed resources");
       }
       onSuccess?.();
+      hotToast.success(i18n("explorer.trash.toast"));
     } catch (e) {
       // TODO display an alert?
       console.error(e);
