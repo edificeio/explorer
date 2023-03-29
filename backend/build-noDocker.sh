7772b2897a98acd68d4de3a67735eb52669b1696
Clement Creusat <clement.creusat@gmail.com>
2023-03-29 14:29:58 +0200

    chore: #WB2-586, clean explorer backend public src

--- backend/build-noDocker.sh
+++ backend/build-noDocker.sh
@@ -43,39 +43,39 @@ clean () {
   gradle clean
 }
 
-buildNode () {
-  #jenkins
-  echo "[buildNode] Get branch name from jenkins env..."
-  BRANCH_NAME=`echo $GIT_BRANCH | sed -e "s|origin/||g"`
-  if [ "$BRANCH_NAME" = "" ]; then
-    echo "[buildNode] Get branch name from git..."
-    BRANCH_NAME=`git branch | sed -n -e "s/^\* \(.*\)/\1/p"`
-  fi
-  if [ "$BRANCH_NAME" = "" ]; then
-    echo "[buildNode] Branch name should not be empty!"
-    exit -1
-  fi
+# buildNode () {
+#   #jenkins
+#   echo "[buildNode] Get branch name from jenkins env..."
+#   BRANCH_NAME=`echo $GIT_BRANCH | sed -e "s|origin/||g"`
+#   if [ "$BRANCH_NAME" = "" ]; then
+#     echo "[buildNode] Get branch name from git..."
+#     BRANCH_NAME=`git branch | sed -n -e "s/^\* \(.*\)/\1/p"`
+#   fi
+#   if [ "$BRANCH_NAME" = "" ]; then
+#     echo "[buildNode] Branch name should not be empty!"
+#     exit -1
+#   fi
 
-  if [ "$BRANCH_NAME" = 'master' ]; then
-      echo "[buildNode] Use entcore version from package.json ($BRANCH_NAME)"
-      case `uname -s` in
-        MINGW*)
-          yarn install --no-bin-links && npm update entcore && node_modules/gulp/bin/gulp.js build
-          ;;
-        *)
-          yarn install && npm update entcore && node_modules/gulp/bin/gulp.js build
-      esac
-  else
-      echo "[buildNode] Use entcore tag $BRANCH_NAME"
-      case `uname -s` in
-        MINGW*)
-          yarn install --no-bin-links && npm rm --no-save entcore && yarn install --no-save entcore@$BRANCH_NAME && node_modules/gulp/bin/gulp.js build
-          ;;
-        *)
-          yarn install && npm rm --no-save entcore && yarn install --no-save entcore@$BRANCH_NAME && node_modules/gulp/bin/gulp.js build
-      esac
-  fi
-}
+#   if [ "$BRANCH_NAME" = 'master' ]; then
+#       echo "[buildNode] Use entcore version from package.json ($BRANCH_NAME)"
+#       case `uname -s` in
+#         MINGW*)
+#           yarn install --no-bin-links && npm update entcore && node_modules/gulp/bin/gulp.js build
+#           ;;
+#         *)
+#           yarn install && npm update entcore && node_modules/gulp/bin/gulp.js build
+#       esac
+#   else
+#       echo "[buildNode] Use entcore tag $BRANCH_NAME"
+#       case `uname -s` in
+#         MINGW*)
+#           yarn install --no-bin-links && npm rm --no-save entcore && yarn install --no-save entcore@$BRANCH_NAME && node_modules/gulp/bin/gulp.js build
+#           ;;
+#         *)
+#           yarn install && npm rm --no-save entcore && yarn install --no-save entcore@$BRANCH_NAME && node_modules/gulp/bin/gulp.js build
+#       esac
+#   fi
+# }
 
 buildGradle () {
   gradle shadowJar install publishToMavenLocal
@@ -92,15 +92,15 @@ publish () {
   gradle publish
 }
 
-buildStatic () {
-    yarn install --no-save ode-bootstrap-neo@feat-explorer && npm run dev:build
-}
+# buildStatic () {
+#     yarn install --no-save ode-bootstrap-neo@feat-explorer && npm run dev:build
+# }
 
-watch () {
-  BUILD_APP="$(prop 'modowner')~$(prop 'modname')~$(prop 'version')"
-  echo "Watching app $BUILD_APP"
-  npm run watch --springboard=/home/node/$SPRINGBOARD --app=\"$BUILD_APP\"
-}
+# watch () {
+#   BUILD_APP="$(prop 'modowner')~$(prop 'modname')~$(prop 'version')"
+#   echo "Watching app $BUILD_APP"
+#   npm run watch --springboard=/home/node/$SPRINGBOARD --app=\"$BUILD_APP\"
+# }
 
 for param in "$@"
 do
@@ -108,18 +108,18 @@ do
     clean)
       clean
       ;;
-    buildNode)
-      buildNode
-      ;;
+    # buildNode)
+    #   buildNode
+    #   ;;
     buildGradle)
       buildGradle
       ;;
     install)
       buildGradle
       ;;
-    watch)
-      watch
-      ;;
+    # watch)
+    #   watch
+    #   ;;
     publish)
       publish
       ;;
