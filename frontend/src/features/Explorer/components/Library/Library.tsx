56f816db5bf15f1af27d733ef615fc1544e92368
david-cc <david.caudeli@web-education.net>
2023-06-14 13:29:48 +0200

    fix: #WB2-887 #WB2-20, set connector URL for Library promotion component

--- frontend/src/features/Explorer/components/Library/Library.tsx
+++ frontend/src/features/Explorer/components/Library/Library.tsx
@@ -1,21 +1,16 @@
 import { Library as CoreLibrary } from "@ode-react-ui/components";
 import { useOdeClient } from "@ode-react-ui/core";
 
-import { capitalizeFirstLetter } from "~/shared/utils/capitalizeFirstLetter";
-import { getAppParams } from "~/shared/utils/getAppParams";
+import { useLibraryUrl } from "./useLibraryUrl";
 
 const Library = () => {
-  const params = getAppParams();
   const { i18n, theme } = useOdeClient();
-
-  const LIB_URL = `https://library.opendigitaleducation.com/search/?application%5B0%5D=${capitalizeFirstLetter(
-    params.app,
-  )}&page=1&sort_field=views&sort_order=desc`;
+  const { libraryUrl } = useLibraryUrl();
 
   return (
     <CoreLibrary
       src={`${theme?.bootstrapPath}/images/image-library.svg`}
-      url={LIB_URL}
+      url={libraryUrl}
       alt={i18n("explorer.libray.img.alt")}
       text={i18n("explorer.libray.title")}
       textButton={i18n("explorer.libray.btn")}
---
+++ frontend/src/features/Explorer/components/Library/useLibraryUrl.tsx
@@ -0,0 +1,26 @@
+import { useOdeClient } from "@ode-react-ui/core";
+import { IUserInfo } from "ode-ts-client";
+
+import { capitalizeFirstLetter } from "~/shared/utils/capitalizeFirstLetter";
+import { getAppParams } from "~/shared/utils/getAppParams";
+
+export const useLibraryUrl = () => {
+  const { user } = useOdeClient();
+  const appName = capitalizeFirstLetter(getAppParams().app);
+
+  // libraryUrl from userInfo.apps is like: https://libraryHost/?platformURL=userPlatformURL
+  const libraryUrlSplitted = (user as IUserInfo).apps
+    .find(
+      (app) =>
+        app.isExternal &&
+        app.address.includes("library") &&
+        app.name.includes("library"),
+    )
+    ?.address.split("?");
+  const libraryHost = libraryUrlSplitted?.[0];
+  const platformParam = libraryUrlSplitted?.[1];
+  const searchParams = `application%5B0%5D=${appName}&page=1&sort_field=views&sort_order=desc`;
+  const libraryUrl = `${libraryHost}search/?${platformParam}&${searchParams}`;
+
+  return { libraryUrl };
+};
