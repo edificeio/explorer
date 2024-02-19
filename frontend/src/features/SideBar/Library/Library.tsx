cb2b577d2aa0cbb1fd7f85122f60c5245d776b3d
david-cc <david.caudeli@web-education.net>
2024-02-19 18:13:58 +0100

    fix: #WB2-1344, check if library URL is set to show Library component

--- frontend/src/features/SideBar/Library/Library.tsx
+++ frontend/src/features/SideBar/Library/Library.tsx
@@ -5,30 +5,32 @@ import { useTranslation } from "react-i18next";
 const Library = () => {
   const { t } = useTranslation();
   const { theme } = useOdeTheme();
-  const { libraryUrl } = useLibraryUrl();
+  const libraryUrl = useLibraryUrl();
   const [imagePath] = usePaths();
 
   return (
-    <div className="p-16">
-      <Image
-        width="270"
-        height="140"
-        loading="lazy"
-        className="rounded"
-        src={`${imagePath}/${theme?.bootstrapVersion}/image-library.svg`}
-        alt={t("explorer.libray.img.alt")}
-      />
-      <p className="m-8">{t("explorer.libray.title")}</p>
-      <a
-        href={libraryUrl}
-        target="_blank"
-        rel="noreferrer"
-        className="d-inline-flex gap-4 btn btn-ghost-primary py-0 p-0 pe-8"
-      >
-        <ArrowRight />
-        <span>{t("explorer.libray.btn")}</span>
-      </a>
-    </div>
+    libraryUrl && (
+      <div className="p-16">
+        <Image
+          width="270"
+          height="140"
+          loading="lazy"
+          className="rounded"
+          src={`${imagePath}/${theme?.bootstrapVersion}/image-library.svg`}
+          alt={t("explorer.libray.img.alt")}
+        />
+        <p className="m-8">{t("explorer.libray.title")}</p>
+        <a
+          href={libraryUrl}
+          target="_blank"
+          rel="noreferrer"
+          className="d-inline-flex gap-4 btn btn-ghost-primary py-0 p-0 pe-8"
+        >
+          <span>{t("explorer.libray.btn")}</span>
+          <ArrowRight />
+        </a>
+      </div>
+    )
   );
 };
 
