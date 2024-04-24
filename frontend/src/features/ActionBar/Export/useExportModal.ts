cab54ecb20b147d4ae4a812fc97ac59a3f9ab825
david-cc <david.caudeli@web-education.net>
2024-04-24 16:27:42 +0200

    fix: #WB2-1424, add Export Modal

---
+++ frontend/src/features/ActionBar/Export/useExportModal.ts
@@ -0,0 +1,25 @@
+import { useToast } from "@edifice-ui/react";
+import { useTranslation } from "react-i18next";
+
+import { goToExport } from "~/services/api";
+import { useSearchParams, useSelectedResources } from "~/store";
+
+export const useExportModal = (onSuccess: () => void) => {
+  const selectedResources = useSelectedResources();
+  const searchParams = useSearchParams();
+
+  const toast = useToast();
+
+  const { t } = useTranslation();
+
+  const handleExportClick = () => {
+    if (selectedResources.length) {
+      goToExport({ searchParams, assetId: selectedResources[0].assetId });
+      onSuccess();
+    } else {
+      toast.error(t("explorer.error.noResourceSelected"));
+    }
+  };
+
+  return { handleExportClick };
+};
